2316a92c2e25632213ffc8f6498a3aa185e59308
CQMian <harryxiang2018@gmail.com>
2022-09-17 11:01:50 -0400

    ..

--- src/Components/Water.js
+++ src/Components/Water.js
@@ -6,8 +6,11 @@ import { useState, useEffect } from "react";
     alert("Go drink some water");
   }, 3000);
 };*/
-
-
+function setAlert(){
+  alert("go drik some WATER")
+}
+setAlert();
+var streak = 0;
 
 function Water() {
   
@@ -17,12 +20,31 @@ const [seconds, setSeconds] = useState(10);
       setTimeout(() => setSeconds(seconds - 1), 1000)
     }
     else{
-      alert("Go Drink Some Water")
+      setAlert();
     }
   },[seconds])
   return (
-    <div>Water
+    <div className='background'>
+      Water
       <h1>Timer: {Math.floor(seconds / 60)} :: {seconds % 60}</h1>
+      <h1 className='watertitle'>
+        <br/>
+        Water Title
+      </h1>
+      <p1>
+      Streak Number: {streak}  
+      </p1>
+     
+      {/* var counter = 0;
+      while(counter!=100){
+        counter++;
+        
+      } */}
+      <p>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          Progress bar
+      </p>
+      <p>This is another paragraph.</p> 
     </div>
   )
 }
