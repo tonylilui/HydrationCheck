802dafca4bd5456c6303c227ea84789623b13f4d
Count-T <tony.lilui416@gmail.com>
2022-09-17 11:06:22 -0400

    added water emoji

--- src/Components/Water.js
+++ src/Components/Water.js
@@ -17,7 +17,7 @@ const [seconds, setSeconds] = useState(10);
       setTimeout(() => setSeconds(seconds - 1), 1000)
     }
     else{
-      alert("Go Drink Some Water")
+      alert("Go Drink Some Water 💧💧💧💧💧💧💧")
     }
   },[seconds])
   return (
