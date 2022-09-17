419544023c0cef046e9b71b21aecacfe5a828637
Pdada1 <pranav.mahabal@gmail.com>
2022-09-17 02:55:47 -0400

    Edited About

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 function About() {
   return (
-    <html>
-      <head>
-        <title>About</title>
-      </head>
-      <body>
-        <h1>Purpose</h1>
-      </body>
-    </html>
+    <div>
+        <h1>About Livewell</h1>
+          <h2>Inspiration</h2>
+            <p>gygvyvtfvg</p>
+          <h2>Functionality</h2>
+            <p>jhdakjhfdakshf</p>
+          <h2>Challenges we faced</h2>
+            <p>adjfhadjhakjhfadfsa</p>
+          <h2>What we learned</h2>
+            <p>adsfkjhadls</p>
 
 
+    </div>
   )
 }
 
